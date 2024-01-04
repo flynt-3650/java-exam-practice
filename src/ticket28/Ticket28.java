17cb84dabaa1ff3a81c5eb44a7f465c1cf27b3d7
Andrew <62994051+Rarmash@users.noreply.github.com>
2024-01-04 13:59:52 +0300

    Ticket28

---
+++ src/ticket28/Ticket28.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) fl_3650.
+ */
+
+package ticket28;
+
+class Ticket28 {
+    public static int sumOfDigits(int n) {
+        if (n == 0) {
+            return 0;
+        } else {
+            return (n % 10) + sumOfDigits(n / 10);
+        }
+    }
+
+    public static void main(String[] args) {
+        int num = 1234567;
+        int sum = sumOfDigits(num);
+        System.out.println("Sum of digits: " + sum);
+    }
+}
