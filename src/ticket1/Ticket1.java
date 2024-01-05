4f65fbf4e406815299e8ba9f4bd3b50060562cf4
fl_3650 <repilovi@gmail.com>
2024-01-05 12:11:13 +0300

    upcasted ArrayLists to Lists

--- src/ticket1/Ticket1.java
+++ src/ticket1/Ticket1.java
@@ -7,42 +7,42 @@ package ticket1;
 import java.util.*;
 
 class Ticket1 {
-    public static ArrayList<Integer> alternate(int[] l1, int[] l2) {
-        ArrayList<Integer> result = new ArrayList<>();
-        if (l1.length == 0 && l2.length == 0) {
+    public static List<Integer> alternate(List<Integer> l1, List<Integer> l2) {
+        List<Integer> result = new ArrayList<>();
+        if (l1.isEmpty() && l2.isEmpty()) {
             return result;
         }
 
-        if (l1.length == l2.length) {
-            for (int i = 0; i < l1.length; ++i) {
-                result.add(l1[i]);
-                result.add(l2[i]);
+        if (l1.size() == l2.size()) {
+            for (int i = 0; i < l1.size(); ++i) {
+                result.add(l1.get(i));
+                result.add(l2.get(i));
             }
-        } else if (l1.length > l2.length) {
+        } else if (l1.size() > l2.size()) {
             int i = 0;
-            for (;i < l2.length; ++i) {
-                result.add(l1[i]);
-                result.add(l2[i]);
+            for (; i < l2.size(); ++i) {
+                result.add(l1.get(i));
+                result.add(l2.get(i));
             }
-            for (; i < l1.length; ++i) {
-                result.add(l1[i]);
+            for (; i < l1.size(); ++i) {
+                result.add(l1.get(i));
             }
         } else {
             int i = 0;
-            for (;i < l1.length; ++i) {
-                result.add(l1[i]);
-                result.add(l2[i]);
+            for (; i < l1.size(); ++i) {
+                result.add(l1.get(i));
+                result.add(l2.get(i));
             }
-            for (; i < l2.length; ++i) {
-                result.add(l2[i]);
+            for (; i < l2.size(); ++i) {
+                result.add(l2.get(i));
             }
         }
         return result;
     }
 
     public static void main(String[] args) {
-        int[] l1 = {1, 2, 3, 4, 5};
-        int[] l2 = {6, 7, 8, 9, 10};
+        List<Integer> l1 = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5));
+        List<Integer> l2 = new ArrayList<>(Arrays.asList(6, 7, 8, 9, 10));
 
         for (var item : alternate(l1, l2))
             System.out.print(item + " ");
