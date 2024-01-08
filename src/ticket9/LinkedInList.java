605ac289ace7c4e6811df846212b0bd973020c5a
lui0000 <elbondarenko04@gmail.com>
2024-01-08 15:29:50 +0300

    final version

--- src/ticket9/LinkedInList.java
+++ src/ticket9/LinkedInList.java
@@ -4,5 +4,74 @@
 
 package ticket9;
 
-public class LinkedInList {
+
+class LinkedInList {
+   ListNode first;
+
+    public void removeDuplicates() {
+        ListNode current = first;
+        while (current != null) {
+            ListNode prev = current;
+            ListNode helper = current.next;
+
+            while (helper != null) {
+                if (helper.val == current.val) {
+                    prev.next = helper.next;
+                } else {
+                    prev = helper;
+                }
+                helper = helper.next;
+            }
+            current = current.next;
+        }
+    }
+
+
+    public void addToList(int val) {
+       ListNode listNode = new ListNode(val);
+       ListNode last = first;
+        if (first == null) {
+            first = listNode;
+        } else {
+            while (last.next != null) {
+                last = last.next;
+            }
+            last.next = listNode;
+        }
+
+    }
+
+    public void printList() {
+       ListNode current = first;
+        while (current != null) {
+            System.out.print(current.val + " ");
+            current = current.next;
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        LinkedInList linkedIntList1 = new LinkedInList();
+        linkedIntList1.addToList(14);
+        linkedIntList1.addToList(8);
+        linkedIntList1.addToList(14);
+        linkedIntList1.addToList(12);
+        linkedIntList1.addToList(1);
+        linkedIntList1.addToList(14);
+        linkedIntList1.addToList(11);
+        linkedIntList1.addToList(8);
+        linkedIntList1.addToList(8);
+        linkedIntList1.addToList(10);
+        linkedIntList1.addToList(4);
+        linkedIntList1.addToList(9);
+        linkedIntList1.addToList(1);
+        linkedIntList1.addToList(2);
+        linkedIntList1.addToList(5);
+        linkedIntList1.addToList(2);
+        linkedIntList1.addToList(4);
+        linkedIntList1.addToList(12);
+        linkedIntList1.addToList(12);
+        linkedIntList1.printList();
+        linkedIntList1.removeDuplicates();
+        linkedIntList1.printList();
+    }
 }
--- src/ticket9/ListNode.java
+++ src/ticket9/ListNode.java
@@ -5,4 +5,9 @@
 package ticket9;
 
 public class ListNode {
+    int val;
+    ListNode next;
+    public ListNode(int val) {
+        this.val = val;
+    }
 }
