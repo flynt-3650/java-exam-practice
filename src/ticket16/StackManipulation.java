eb1f4e4cec63406273543945b034a98da2cc41a3
lui0000 <elbondarenko04@gmail.com>
2024-01-08 22:30:36 +0300

    added 16 ticket

--- src/ticket16/StackManipulation.java
+++ src/ticket16/StackManipulation.java
@@ -1,8 +1,45 @@
 /*
- * Copyright (c) fl_3650.
+ * Copyright (c) Eliza
  */
 
 package ticket16;
+import java.util.Stack;
+class StackManipulation {
+    public Stack<Integer> copyStack(Stack<Integer> stack) {
+        Stack<Integer> stackHelper = new Stack<>();
+        Stack<Integer> stackFinal = new Stack<>();
+        while(!stack.isEmpty()) {
+            stackHelper.push(stack.pop());
+        }
+        while (!stackHelper.isEmpty()) {
+            int element = stackHelper.pop();
+            stack.push(element); // восстановление изначального стека (не обязательно)
+            stackFinal.push(element);
+        }
+        return stackFinal;
+    }
 
-public class StackManipulation {
+    public static void main(String[] args) {
+        StackManipulation stackManipulation = new StackManipulation();
+        Stack<Integer> stack = new Stack<>();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+        stack.push(5);
+        // Копирование стека
+        Stack<Integer> copiedStack = stackManipulation.copyStack(stack);
+        System.out.println("origin stack");
+        for(Integer element: stack) {
+            System.out.print(element + " ");
+
+        }
+        System.out.println("\nstack after using copyStack");
+        for(Integer element: copiedStack) {
+            System.out.print(element + " ");
+        }
+//        System.out.println(stack); так тоже можно печатать
+//        System.out.println(copiedStack);
+    }
+
 }
