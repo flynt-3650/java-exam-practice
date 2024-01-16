1307fab9269e1763ce8ded3700725344a4e70132
lui0000 <elbondarenko04@gmail.com>
2024-01-16 20:46:46 +0300

    added ticket 10

---
+++ src/ticket10/Ticket10.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) Eliza
+ */
+
+package ticket10;
+
+import java.util.Iterator;
+import java.util.Stack;
+
+class Ticket10 {
+    public static Stack<Integer> splitStack(Stack<Integer> stack){
+        Stack<Integer> positiveNumbers = new Stack<>();
+        Stack<Integer> negativeNumbers = new Stack<>();
+        Iterator<Integer> iterator = stack.iterator();
+        while (iterator.hasNext()){
+            Integer number = iterator.next();
+            if(number < 0){
+                negativeNumbers.push(number);
+                iterator.remove();
+
+            } else {
+                positiveNumbers.push(number);
+                iterator.remove();
+
+            }
+        }
+
+
+        stack.addAll(negativeNumbers);
+        stack.addAll(positiveNumbers);
+
+        return stack;
+    }
+
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(-1);
+        stack.push(2);
+        stack.push(-8);
+        stack.push(10);
+        stack.push(11);
+        stack.push(0);
+        stack.push(-6);
+        stack.push(-95);
+        System.out.println( splitStack(stack));
+
+    }
+}
