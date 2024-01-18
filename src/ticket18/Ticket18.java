fc274833ee7d859d6aff42349748fbd9f7a0359a
Kirill Pavlov <kirillpavlov4214@gmail.com>
2024-01-18 14:28:38 +0300

    fixed 18th ticket solution

--- src/ticket18/Ticket18.java
+++ src/ticket18/Ticket18.java
@@ -6,10 +6,19 @@ package ticket18;
 
 class Ticket18 {
     public static void main(String[] args) {
-        int[] arr = {1, 3, 5, 2, 9, 7, -3, 0, 42, 308, 17};
         ArrayIntList ail = new ArrayIntList();
-        ail.setElementData(arr);
-        System.out.println(ail.longestSortedSequence());
+
+        ail.setElementData(new int[]{1, 3, 5, 2, 9, 7, -3, 0, 42, 308, 17});
+        System.out.println(ail.longestSortedSequence()); // 4 (-3, 0, 42, 308)
+
+        ail.setElementData(new int[]{1, 3, 5, 2, 9, 7, -3, 0, 42, 308, 17, 5, 4, 3});
+        System.out.println(ail.longestSortedSequence()); // 5 (308, 17, 5, 4, 3)
+
+        ail.setElementData(new int[]{1});
+        System.out.println(ail.longestSortedSequence()); // 1 (1)
+
+        ail.setElementData(new int[]{});
+        System.out.println(ail.longestSortedSequence()); // 0 ()
     }
 
 }
